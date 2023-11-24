771cd3203ef9fabf47f81810f5ccd909c55e817a
Kévin Casagrande <github.kcasagrande@xoxy.net>
2023-11-24 15:10:15 +0100

    Reimplement 1D Spreadsheet with caching

--- src/main/scala/codingame/training/easy/onedspreadsheet/Solution.scala
+++ src/main/scala/codingame/training/easy/onedspreadsheet/Solution.scala
@@ -2,78 +2,97 @@ package codingame.training.easy.onedspreadsheet
 
 import scala.io.StdIn._
 
+case class WithCache[T](t: T, cache: Map[Int, Int])
+
 sealed trait Operand {
-    def value(map: Map[Int, Operation]): Int
+  def computeValue(cache: Map[Int, Int], operations: Seq[Operation]): WithCache[Int]
 }
 object Operand {
-    def apply(token: String): Operand = {
-        token match {
-            case Value.regex(value) => Value(value.toInt)
-            case Reference.regex(cell) => Reference(cell.toInt)
-        }
+  def apply(token: String): Operand = {
+    token match {
+      case Value.regex(value) => Value(value.toInt)
+      case Reference.regex(cell) => Reference(cell.toInt)
     }
-    final case class Value(private val _value: Int) extends Operand {
-        override def value(map: Map[Int, Operation]): Int = _value
+  }
+  final case class Value(private val value: Int) extends Operand {
+    def computeValue(cache: Map[Int, Int], operations: Seq[Operation]): WithCache[Int] =
+      WithCache(value, cache)
+  }
+  object Value {
+    val regex = "^(0|-?[1-9][0-9]*)$".r
+  }
+  final case class Reference(cell: Int) extends Operand {
+    def computeValue(cache: Map[Int, Int], operations: Seq[Operation]): WithCache[Int] = {
+      cache.get(cell).map(WithCache(_, cache)).getOrElse {
+        val result = operations(cell).computeValue(cache, operations)
+        WithCache(result.t, result.cache + (cell -> result.t))
+      }
     }
-    object Value {
-        val regex = "^(0|-?[1-9][0-9]*)$".r
-    }
-    final case class Reference(cell: Int) extends Operand {
-        override def value(map: Map[Int, Operation]): Int = map(cell).value(map)
-    }
-    object Reference {
-        val regex = "^\\$(0|[1-9][0-9]*)$".r
-    }
+  }
+  object Reference {
+    val regex = "^\\$(0|[1-9][0-9]*)$".r
+  }
 }
 
 sealed trait Operation {
-    def value(map: Map[Int, Operation]): Int
+  def index: Int
+  def computeValue(cache: Map[Int, Int], operations: Seq[Operation]): WithCache[Int]
 }
 object Operation {
-    val operandRegex = "[^\\s]+"
+  final case class Value(index: Int, operand: Operand) extends Operation {
+    override def computeValue(cache: Map[Int, Int], operations: Seq[Operation]): WithCache[Int] = {
+      val result = operand.computeValue(cache, operations)
+      WithCache(result.t, result.cache + (index -> result.t))
+    }
+  }
+  final case class Add(index: Int, operand1: Operand, operand2: Operand) extends Operation {
+    override def computeValue(cache: Map[Int, Int], operations: Seq[Operation]): WithCache[Int] = {
+      val result1 = operand1.computeValue(cache, operations)
+      val result2 = operand2.computeValue(result1.cache, operations)
+      val result = result1.t + result2.t
+      WithCache(result, result2.cache + (index -> result))
+    }
+  }
+  final case class Sub(index: Int, operand1: Operand, operand2: Operand) extends Operation {
+    override def computeValue(cache: Map[Int, Int], operations: Seq[Operation]): WithCache[Int] = {
+      val result1 = operand1.computeValue(cache, operations)
+      val result2 = operand2.computeValue(result1.cache, operations)
+      val result = result1.t - result2.t
+      WithCache(result, result2.cache + (index -> result))
+    }
+  }
+  final case class Mult(index: Int, operand1: Operand, operand2: Operand) extends Operation {
+    override def computeValue(cache: Map[Int, Int], operations: Seq[Operation]): WithCache[Int] = {
+      val result1 = operand1.computeValue(cache, operations)
+      val result2 = operand2.computeValue(result1.cache, operations)
+      val result = result1.t * result2.t
+      WithCache(result, result2.cache + (index -> result))
+    }
+  }
 
-    def apply(line: String): Operation = {
-        line match {
-            case Value.regex(operand) => Value(Operand(operand))
-            case Add.regex(operand1, operand2) => Add(Operand(operand1), Operand(operand2))
-            case Sub.regex(operand1, operand2) => Sub(Operand(operand1), Operand(operand2))
-            case Mult.regex(operand1, operand2) => Mult(Operand(operand1), Operand(operand2))
-        }
-    }
-    final case class Value(operand: Operand) extends Operation {
-        override def value(map: Map[Int, Operation]): Int = operand.value(map)
-    }
-    object Value {
-        val regex = s"^VALUE ($operandRegex) $operandRegex$$".r
-    }
-    final case class Add(operand1: Operand, operand2: Operand) extends Operation {
-        override def value(map: Map[Int, Operation]): Int = operand1.value(map) + operand2.value(map)
-    }
-    object Add {
-        val regex = s"^ADD ($operandRegex) ($operandRegex)$$".r
-    }
-    final case class Sub(operand1: Operand, operand2: Operand) extends Operation {
-        override def value(map: Map[Int, Operation]): Int = operand1.value(map) - operand2.value(map)
-    }
-    object Sub {
-        val regex = s"^SUB ($operandRegex) ($operandRegex)$$".r
-    }
-    final case class Mult(operand1: Operand, operand2: Operand) extends Operation {
-        override def value(map: Map[Int, Operation]): Int = operand1.value(map) * operand2.value(map)
-    }
-    object Mult {
-        val regex = s"^MULT ($operandRegex) ($operandRegex)$$".r
-    }
+  def parse(index: Int, tokens: Seq[String]): Operation = tokens match {
+    case "VALUE" :: operand :: _ => Operation.Value(index, Operand(operand))
+    case "ADD" :: operand1 :: operand2 :: _ => Operation.Add(index, Operand(operand1), Operand(operand2))
+    case "SUB" :: operand1 :: operand2 :: _ => Operation.Sub(index, Operand(operand1), Operand(operand2))
+    case "MULT" :: operand1 :: operand2 :: _ => Operation.Mult(index, Operand(operand1), Operand(operand2))
+  }
 }
 
 object Solution extends App {
-    val n = readLine.toInt
-    val map: Map[Int, Operation] = LazyList.fill(n)(readLine)
-        .zipWithIndex
-        .map(_.swap)
-        .toMap
-        .map(entry => entry._1 -> Operation(entry._2))
-    map.keys.toSeq.sorted
-        .map(map(_).value(map))
-        .foreach(println)
+  val n = readLine.toInt
+  val operations: Seq[Operation] =
+    LazyList.fill(n)(readLine)
+      .map(_.split(" ").toList)
+      .zipWithIndex
+      .map {
+        case (tokens, index) => Operation.parse(index, tokens)
+      }
+  val cache = Map.empty[Int, Int]
+  operations
+    .foldLeft(cache)((currentCache, operation) => operation.computeValue(currentCache, operations).cache)
+    .toSeq
+    .sortBy(_._1)
+    .map(_._2)
+    .foreach(println)
+
 }
--- src/test/scala/codingame/training/easy/onedspreadsheet/Test.scala
+++ src/test/scala/codingame/training/easy/onedspreadsheet/Test.scala
@@ -6,42 +6,295 @@ import org.scalatest.matchers.should.Matchers._
 
 class Test
   extends AnyFreeSpecLike
-  with InOutTesting
-{
+    with InOutTesting {
   "1D Spreadsheet" - {
     implicit val app: App = Solution
 
-    "Simple dependency" in outputWithInput(
-      """2
-        |VALUE 3 _
-        |ADD $0 4"""
-        .stripMargin
-    ) { _ shouldEqual
-        """3
-          |7
-          |""".stripMargin
+    "TDD" - {
+      "One simple value" in outputWithInput(
+        """1
+          |VALUE 2 _
+          |"""
+          .stripMargin
+      ) {
+        _ shouldEqual
+          """2
+            |""".stripMargin
+      }
+
+      "One simple add" in outputWithInput(
+        """1
+          |ADD 2 3
+          |"""
+          .stripMargin
+      ) {
+        _ shouldEqual
+          """5
+            |""".stripMargin
+      }
+
+      "One simple sub" in outputWithInput(
+        """1
+          |SUB 2 3
+          |"""
+          .stripMargin
+      ) {
+        _ shouldEqual
+          """-1
+            |""".stripMargin
+      }
+
+      "One simple mult" in outputWithInput(
+        """1
+          |MULT 2 3
+          |"""
+          .stripMargin
+      ) {
+        _ shouldEqual
+          """6
+            |""".stripMargin
+      }
+
+      "Simple dependency" in outputWithInput(
+        """2
+          |VALUE 2 _
+          |VALUE $0 _
+          |"""
+          .stripMargin
+      ) {
+        _ shouldEqual
+          """2
+            |2
+            |""".stripMargin
+      }
     }
 
-    "Coefficients" in outputWithInput(
-      """7
-        |VALUE 10 _
-        |VALUE 3 _
-        |MULT $0 $1
-        |VALUE 2 _
-        |VALUE 4 _
-        |MULT $3 $4
-        |ADD $2 $5"""
-        .stripMargin
-    ) {
-      _ shouldEqual
-        """10
-          |3
-          |30
-          |2
-          |4
-          |8
-          |38
+    "Codingame test cases" - {
+      "Simple dependency" in outputWithInput(
+        """2
+          |VALUE 3 _
+          |ADD $0 4"""
+          .stripMargin
+      ) {
+        _ shouldEqual
+          """3
+            |7
+            |""".stripMargin
+      }
+
+      "Coefficients" in outputWithInput(
+        """7
+          |VALUE 10 _
+          |VALUE 3 _
+          |MULT $0 $1
+          |VALUE 2 _
+          |VALUE 4 _
+          |MULT $3 $4
+          |ADD $2 $5"""
+          .stripMargin
+      ) {
+        _ shouldEqual
+          """10
+            |3
+            |30
+            |2
+            |4
+            |8
+            |38
+            |""".stripMargin
+      }
+
+      "Deep Birecursion" in outputWithInput(
+        """92
+          |SUB $33 $64
+          |ADD $60 $60
+          |ADD $61 $61
+          |SUB $76 $80
+          |SUB $25 $59
+          |ADD $58 $28
+          |ADD $88 $59
+          |ADD $32 $32
+          |ADD $83 $21
+          |ADD $69 $39
+          |ADD $57 $64
+          |ADD $26 $26
+          |ADD $1 $1
+          |SUB $62 $68
+          |ADD $73 $1
+          |ADD $50 $27
+          |SUB $24 $2
+          |ADD $14 $12
+          |ADD $10 $89
+          |SUB $67 $35
+          |ADD $58 $58
+          |ADD $7 $7
+          |SUB $0 $89
+          |ADD $20 $20
+          |SUB $43 $61
+          |SUB $53 $11
+          |ADD $37 $37
+          |ADD $82 $47
+          |ADD $90 $2
+          |ADD $89 $89
+          |ADD $85 $85
+          |SUB $91 $47
+          |ADD $69 $69
+          |SUB $46 $86
+          |SUB $42 $20
+          |ADD $12 $12
+          |ADD $56 $8
+          |ADD $72 $72
+          |ADD $9 $32
+          |ADD $30 $77
+          |ADD $80 $48
+          |ADD $79 $81
+          |SUB $16 $58
+          |SUB $44 $56
+          |SUB $63 $21
+          |ADD $20 $5
+          |SUB $49 $81
+          |ADD $54 $54
+          |ADD $29 $18
+          |SUB $34 $23
+          |ADD $47 $47
+          |SUB $74 $32
+          |SUB $17 $72
+          |SUB $71 $26
+          |ADD $59 $59
+          |ADD $15 $68
+          |ADD $21 $21
+          |ADD $86 $41
+          |ADD $2 $2
+          |ADD $11 $11
+          |ADD $80 $80
+          |ADD $56 $56
+          |SUB $31 $50
+          |SUB $51 $7
+          |ADD $86 $86
+          |ADD $72 $35
+          |SUB $75 $30
+          |SUB $70 $12
+          |ADD $50 $50
+          |ADD $30 $30
+          |SUB $84 $1
+          |SUB $52 $37
+          |VALUE 1 _
+          |ADD $40 $60
+          |SUB $66 $69
+          |SUB $13 $85
+          |SUB $22 $29
+          |ADD $55 $85
+          |ADD $37 $65
+          |ADD $23 $45
+          |ADD $29 $29
+          |ADD $23 $23
+          |ADD $54 $6
+          |ADD $38 $7
+          |SUB $3 $60
+          |ADD $68 $68
+          |ADD $81 $81
+          |ADD $78 $26
+          |ADD $87 $11
+          |ADD $64 $64
+          |ADD $61 $36
+          |SUB $4 $54
           |""".stripMargin
+      ) {
+        _ shouldEqual
+          """2130706432
+            |268435456
+            |131072
+            |2013265920
+            |2147483616
+            |1074266111
+            |1073741855
+            |8192
+            |1073774591
+            |1073745919
+            |1090519039
+            |8
+            |536870912
+            |2147483136
+            |1610612735
+            |1073742079
+            |2147221504
+            |2147483647
+            |1107296255
+            |0
+            |524288
+            |16384
+            |2113929216
+            |1048576
+            |2147352576
+            |2147483632
+            |4
+            |1073741951
+            |1074003967
+            |33554432
+            |1024
+            |2147483520
+            |4096
+            |2139095040
+            |2146435072
+            |1073741824
+            |1073807359
+            |2
+            |1073750015
+            |1073743871
+            |1207959551
+            |1077936127
+            |2146959360
+            |2147418112
+            |2147450880
+            |1074790399
+            |2143289344
+            |64
+            |1140850687
+            |2145386496
+            |128
+            |2147475456
+            |2147483646
+            |2147483640
+            |32
+            |1073742335
+            |32768
+            |1082130431
+            |262144
+            |16
+            |134217728
+            |65536
+            |2147483392
+            |2147467264
+            |8388608
+            |1073741825
+            |2147481600
+            |1073741824
+            |256
+            |2048
+            |1610612736
+            |2147483644
+            |1
+            |1342177279
+            |2147479552
+            |2147482624
+            |2080374784
+            |1073742847
+            |1073741827
+            |1075838975
+            |67108864
+            |2097152
+            |1073741887
+            |1073758207
+            |1879048192
+            |512
+            |4194304
+            |1073741831
+            |1073741839
+            |16777216
+            |1073872895
+            |2147483584
+            |""".stripMargin
+      }
     }
   }
 }
