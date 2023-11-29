00e037ee2a93f6adc3958ba94d0782d75f2a4529
Kévin Casagrande <github.kcasagrande@xoxy.net>
2023-11-29 17:06:28 +0100

    TDD - Decode a long message - Refactor

--- src/main/scala/codingame/training/easy/encryptiondecryptionofenigmamachine/Solution.scala
+++ src/main/scala/codingame/training/easy/encryptiondecryptionofenigmamachine/Solution.scala
@@ -2,6 +2,38 @@ package codingame.training.easy.encryptiondecryptionofenigmamachine
 
 import scala.io.StdIn._
 
+sealed trait Operation extends (String => String) {
+  override def apply(message: String): String
+}
+object Operation {
+  private def caesarShift(shift: Int)(implicit alphabet: String): (String) => String = _
+    .map(alphabet.indexOf(_))
+    .zipWithIndex
+    .map(tuple => tuple._1 + tuple._2)
+    .map(_ + shift)
+    .map(_ % alphabet.length)
+    .map(alphabet(_))
+    .mkString
+
+  private def caesarUnshift(shift: Int)(implicit alphabet: String): (String) => String = caesarShift(shift)(alphabet.reverse)
+
+  case class Encode(private val initialShift: Int, private val rotors: Seq[Rotor])(implicit alphabet: String) extends Operation {
+    override def apply(message: String): String = rotors.foldLeft(caesarShift(initialShift)){ (operations, rotor) => operations andThen rotor }(message)
+  }
+
+  case class Decode(private val initialShift: Int, private val rotors: Seq[Rotor])(implicit alphabet: String) extends Operation {
+    override def apply(message: String): String = (rotors.foldRight(identity[String](_)) { (rotor, operations ) => operations andThen rotor.reverse } andThen caesarUnshift(initialShift))(message)
+  }
+
+  def apply(operationAsString: String, initialShift: Int, rotors: Seq[Rotor])(implicit alphabet: String): Operation = {
+    operationAsString match {
+      case "ENCODE" => Encode(initialShift, rotors)
+      case "DECODE" => Decode(initialShift, rotors)
+    }
+  }
+}
+
+
 case class Rotor(configuration: String)(implicit alphabet: String) extends (String => String) {
   override def apply(message: String): String =
     message
@@ -24,23 +56,5 @@ object Solution extends App {
     .map(Rotor(_))
   val message = readLine()
 
-  def caesarShift(shift: Int)(implicit alphabet: String): (String) => String = _
-    .map(alphabet.indexOf(_))
-    .zipWithIndex
-    .map(tuple => tuple._1 + tuple._2)
-    .map(_ + shift)
-    .map(_ % alphabet.length)
-    .map(alphabet(_))
-    .mkString
-
-  def caesarUnshift(shift: Int)(implicit alphabet: String): (String) => String = caesarShift(shift)(alphabet.reverse)
-
-  val encode = rotors.foldLeft(caesarShift(initialShift)) { (operations, rotor) => operations andThen rotor }
-  val decode = rotors.foldRight(identity[String](_)){ (rotor, operations) => operations andThen rotor.reverse } andThen caesarUnshift(initialShift)
-
-  if(operation == "ENCODE") {
-    println(encode(message))
-  } else {
-    println(decode(message))
-  }
+  println(Operation(operation, initialShift, rotors).apply(message))
 }
