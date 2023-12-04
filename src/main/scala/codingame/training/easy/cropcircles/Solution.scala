2b77555e11107dc7869c35213a84c30dc601d677
Kévin Casagrande <github.kcasagrande@xoxy.net>
2023-12-04 22:51:47 +0100

    TDD - Crop circles - Simplest circle - Refactor

--- src/main/scala/codingame/training/easy/cropcircles/Solution.scala
+++ src/main/scala/codingame/training/easy/cropcircles/Solution.scala
@@ -1,13 +1,26 @@
 package codingame.training.easy.cropcircles
 
+import scala.io.StdIn._
+import scala.util.matching.Regex
+
+case class Instruction(x: Int, y: Int)
+object Instruction {
+  val fromInput: Regex = "^([a-z])([a-z])(0|[1-9][0-9]*)$".r
+  def fromMatch(x: String, y: String): Instruction =
+    Instruction(x.head - 'a', y.head - 'a')
+}
+
 object Solution extends App {
+  val instruction = readLine match {
+    case Instruction.fromInput(x, y, _) => Instruction.fromMatch(x, y)
+  }
   val field = (for {
     y <- 0 until 25
   } yield {
     (for {
       x <- 0 until 19
     } yield {
-      if(x == 'o' - 'a' && y == 'e' - 'a') {
+      if(x == instruction.x && y == instruction.y) {
         "  "
       } else {
         "{}"
