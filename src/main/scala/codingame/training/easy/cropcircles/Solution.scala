f2bb92e956befa8941f344d14c5a4656915ab7af
Kévin Casagrande <github.kcasagrande@xoxy.net>
2023-12-04 22:42:33 +0100

    TDD - Crop circles - Simplest circle - Green

--- src/main/scala/codingame/training/easy/cropcircles/Solution.scala
+++ src/main/scala/codingame/training/easy/cropcircles/Solution.scala
@@ -1,5 +1,18 @@
 package codingame.training.easy.cropcircles
 
 object Solution extends App {
-  println
+  val field = (for {
+    y <- 0 until 25
+  } yield {
+    (for {
+      x <- 0 until 19
+    } yield {
+      if(x == 'o' - 'a' && y == 'e' - 'a') {
+        "  "
+      } else {
+        "{}"
+      }
+    }).mkString
+  }).mkString("\n")
+  println(field)
 }
