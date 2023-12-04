a7f6e14c6c0625818dfea6ce3bee9e8d213efb17
Kévin Casagrande <github.kcasagrande@xoxy.net>
2023-12-04 23:56:00 +0100

    TDD - Crop circles - Small circle

--- src/main/scala/codingame/training/easy/cropcircles/Solution.scala
+++ src/main/scala/codingame/training/easy/cropcircles/Solution.scala
@@ -3,16 +3,25 @@ package codingame.training.easy.cropcircles
 import scala.io.StdIn._
 import scala.util.matching.Regex
 
-case class Instruction(x: Int, y: Int)
+case class Point(
+  x: Int,
+  y: Int
+) {
+  def <->(other: Point): Double = Math.sqrt(Math.pow(other.x - x, 2) + Math.pow(other.y - y, 2))
+}
+
+case class Instruction(center: Point, diameter: Int) {
+  def includes(point: Point): Boolean = (point <-> center) <= (diameter / 2)
+}
 object Instruction {
-  val fromInput: Regex = "^([a-z])([a-z])(0|[1-9][0-9]*)$".r
-  def fromMatch(x: String, y: String): Instruction =
-    Instruction(x.head - 'a', y.head - 'a')
+  val fromInput: Regex = "^([a-s])([a-y])(0|[1-9][0-9]*)$".r
+  def fromMatch(x: String, y: String, d: String): Instruction =
+    Instruction(Point(x.head - 'a', y.head - 'a'), d.toInt)
 }
 
 object Solution extends App {
   val instruction = readLine match {
-    case Instruction.fromInput(x, y, _) => Instruction.fromMatch(x, y)
+    case Instruction.fromInput(x, y, d) => Instruction.fromMatch(x, y, d)
   }
   val field = (for {
     y <- 0 until 25
@@ -20,7 +29,7 @@ object Solution extends App {
     (for {
       x <- 0 until 19
     } yield {
-      if(x == instruction.x && y == instruction.y) {
+      if(instruction.includes(Point(x, y))) {
         "  "
       } else {
         "{}"
