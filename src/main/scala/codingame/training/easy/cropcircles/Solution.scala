fc6e703e8576928e9c5641504b6a4a69b4b8a39d
Kévin Casagrande <github.kcasagrande@xoxy.net>
2023-12-06 16:42:03 +0100

    Crop circles - TDD - Plantmow after mow - Green

--- src/main/scala/codingame/training/easy/cropcircles/Solution.scala
+++ src/main/scala/codingame/training/easy/cropcircles/Solution.scala
@@ -1,5 +1,7 @@
 package codingame.training.easy.cropcircles
 
+import codingame.training.easy.cropcircles.State.{Mowed, Planted}
+
 import scala.io.StdIn._
 import scala.util.matching.Regex
 
@@ -10,22 +12,38 @@ case class Point(
   def <->(other: Point): Double = Math.sqrt(Math.pow(other.x - x, 2) + Math.pow(other.y - y, 2))
 }
 
+sealed trait State extends (Instruction => State) {
+  final override def apply(instruction: Instruction): State = instruction match {
+    case Instruction.Plant(_, _) => Planted
+    case Instruction.Mow(_, _) => Mowed
+    case Instruction.PlantMow(_, _) => plantMow
+  }
+  protected def plantMow: State
+}
+object State {
+  case object Planted extends State {
+    override def toString: String = "{}"
+    override protected def plantMow: State = Mowed
+  }
+  case object Mowed extends State {
+    override def toString: String = "  "
+    override protected def plantMow: State = Planted
+  }
+}
+
 sealed abstract class Instruction(center: Point, diameter: Int) {
   def includes(point: Point): Boolean = (point <-> center) <= (diameter / 2.0)
-  def result: String
 }
 object Instruction {
-  case class Mow(center: Point, diameter: Int) extends Instruction(center, diameter) {
-    override val result: String = "  "
-  }
-  case class Plant(center: Point, diameter: Int) extends Instruction(center, diameter) {
-    override val result: String = "{}"
-  }
+  case class Mow(center: Point, diameter: Int) extends Instruction(center, diameter)
+  case class Plant(center: Point, diameter: Int) extends Instruction(center, diameter)
+  case class PlantMow(center: Point, diameter: Int) extends Instruction(center, diameter)
 
-  val fromInput: Regex = "^((?:PLANT)?)([a-s])([a-y])(0|[1-9][0-9]*)$".r
+  val fromInput: Regex = "^((?:PLANT)?|(?:PLANTMOW)?)([a-s])([a-y])(0|[1-9][0-9]*)$".r
   def fromMatch(operation: String, x: String, y: String, d: String): Instruction = {
     operation match {
       case "PLANT" => Plant(Point(x.head - 'a', y.head - 'a'), d.toInt)
+      case "PLANTMOW" => PlantMow(Point(x.head - 'a', y.head - 'a'), d.toInt)
       case _ => Mow(Point(x.head - 'a', y.head - 'a'), d.toInt)
     }
   }
@@ -35,7 +53,7 @@ object Solution extends App {
   val instructions = readLine.split(" ").map {
     case Instruction.fromInput(operation, x, y, d) => Instruction.fromMatch(operation, x, y, d)
   }
-  val initialField = Seq.fill(25)(Seq.fill(19)("{}"))
+  val initialField: Seq[Seq[State]] = Seq.fill(25)(Seq.fill(19)(State.Planted))
   val field = instructions.foldLeft(initialField){(currentField, instruction) =>
     for {
       y <- 0 until 25
@@ -44,12 +62,12 @@ object Solution extends App {
         x <- 0 until 19
       } yield {
         if(instruction.includes(Point(x, y))) {
-          instruction.result
+          currentField(y)(x).apply(instruction)
         } else {
           currentField(y)(x)
         }
       }
     }
   }
-  println(field.map(_.mkString).mkString("\n"))
+  println(field.map(_.map(_.toString).mkString).mkString("\n"))
 }
