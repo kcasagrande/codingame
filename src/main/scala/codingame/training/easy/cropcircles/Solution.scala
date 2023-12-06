17f779ad2c504486dca6210805430d864145bc75
Kévin Casagrande <github.kcasagrande@xoxy.net>
2023-12-06 16:28:56 +0100

    Crop circles - TDD - Plant after mow - Green

--- src/main/scala/codingame/training/easy/cropcircles/Solution.scala
+++ src/main/scala/codingame/training/easy/cropcircles/Solution.scala
@@ -10,31 +10,46 @@ case class Point(
   def <->(other: Point): Double = Math.sqrt(Math.pow(other.x - x, 2) + Math.pow(other.y - y, 2))
 }
 
-case class Instruction(center: Point, diameter: Int) {
+sealed abstract class Instruction(center: Point, diameter: Int) {
   def includes(point: Point): Boolean = (point <-> center) <= (diameter / 2.0)
+  def result: String
 }
 object Instruction {
-  val fromInput: Regex = "^([a-s])([a-y])(0|[1-9][0-9]*)$".r
-  def fromMatch(x: String, y: String, d: String): Instruction =
-    Instruction(Point(x.head - 'a', y.head - 'a'), d.toInt)
+  case class Mow(center: Point, diameter: Int) extends Instruction(center, diameter) {
+    override val result: String = "  "
+  }
+  case class Plant(center: Point, diameter: Int) extends Instruction(center, diameter) {
+    override val result: String = "{}"
+  }
+
+  val fromInput: Regex = "^((?:PLANT)?)([a-s])([a-y])(0|[1-9][0-9]*)$".r
+  def fromMatch(operation: String, x: String, y: String, d: String): Instruction = {
+    operation match {
+      case "PLANT" => Plant(Point(x.head - 'a', y.head - 'a'), d.toInt)
+      case _ => Mow(Point(x.head - 'a', y.head - 'a'), d.toInt)
+    }
+  }
 }
 
 object Solution extends App {
   val instructions = readLine.split(" ").map {
-    case Instruction.fromInput(x, y, d) => Instruction.fromMatch(x, y, d)
+    case Instruction.fromInput(operation, x, y, d) => Instruction.fromMatch(operation, x, y, d)
   }
-  val field = (for {
-    y <- 0 until 25
-  } yield {
-    (for {
-      x <- 0 until 19
+  val initialField = Seq.fill(25)(Seq.fill(19)("{}"))
+  val field = instructions.foldLeft(initialField){(currentField, instruction) =>
+    for {
+      y <- 0 until 25
     } yield {
-      if(instructions.exists(_.includes(Point(x, y)))) {
-        "  "
-      } else {
-        "{}"
+      for {
+        x <- 0 until 19
+      } yield {
+        if(instruction.includes(Point(x, y))) {
+          instruction.result
+        } else {
+          currentField(y)(x)
+        }
       }
-    }).mkString
-  }).mkString("\n")
-  println(field)
+    }
+  }
+  println(field.map(_.mkString).mkString("\n"))
 }
