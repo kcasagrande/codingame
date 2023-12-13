77d6ff47e4d7bcd64289799a081bc30b53c01238
Kévin Casagrande <github.kcasagrande@xoxy.net>
2023-12-13 14:16:36 +0100

    Equivalent resistance circuit building - TDD - Single resistor - Refactor

--- src/main/scala/codingame/training/easy/equivalentresistancecircuitbuilding/Solution.scala
+++ src/main/scala/codingame/training/easy/equivalentresistancecircuitbuilding/Solution.scala
@@ -1,7 +1,12 @@
 package codingame.training.easy.equivalentresistancecircuitbuilding
 
+import java.util.Locale.US
 import scala.io.StdIn._
 
 object Solution extends App {
-  println("1.0")
+  readLine
+  val Array(name, _resistance) = readLine.split(" ")
+  val resistances: Map[String, Float] = Map(name -> _resistance.toFloat)
+  val resistance = resistances("A")
+  println("%.1f".formatLocal(US, resistance))
 }
