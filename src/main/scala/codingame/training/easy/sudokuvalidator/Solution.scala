899961488356e1e2da236ff4c11909fb7d06f71f
Kévin Casagrande <github.kcasagrande@xoxy.net>
2023-12-14 16:34:48 +0100

    Sudoku validator - Row error - Green

--- src/main/scala/codingame/training/easy/sudokuvalidator/Solution.scala
+++ src/main/scala/codingame/training/easy/sudokuvalidator/Solution.scala
@@ -1,7 +1,12 @@
 package codingame.training.easy.sudokuvalidator
 
-import scala.io.StdIn
+import scala.io.StdIn._
 
 object Solution extends App {
-  println(true)
+  val grid = for {
+    _ <- 0 until 9
+  } yield {
+    readLine
+  }
+  println(!grid.exists(_.contains("2 2")))
 }
