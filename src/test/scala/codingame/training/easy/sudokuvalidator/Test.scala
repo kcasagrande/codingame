8ae3ef5281cf7f9cbd246254e67629decd996b5a
Kévin Casagrande <github.kcasagrande@xoxy.net>
2023-12-14 16:45:40 +0100

    Sudoku validator - Non consecutive row error - Red

--- src/test/scala/codingame/training/easy/sudokuvalidator/Test.scala
+++ src/test/scala/codingame/training/easy/sudokuvalidator/Test.scala
@@ -44,5 +44,22 @@ class Test
           |""".stripMargin
     }
 
+    "should detect a non consecutive row error" in outputWithInput(
+      """4 3 2 6 2 9 7 8 1
+        |6 8 5 7 5 1 4 9 3
+        |1 9 7 3 8 4 5 6 2
+        |8 2 6 9 1 5 3 4 7
+        |3 7 4 8 6 2 9 1 5
+        |9 5 1 4 7 3 6 2 8
+        |5 1 9 2 3 6 8 7 4
+        |2 4 8 5 9 7 1 3 6
+        |7 6 3 1 4 8 2 5 9
+        |""".stripMargin
+    ) {
+      _ shouldEqual
+        """false
+          |""".stripMargin
+    }
+
   }
 }
