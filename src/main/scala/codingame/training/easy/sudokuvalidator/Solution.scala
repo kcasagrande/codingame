f7df0c5bb49d5e0462e0a5b3cfd6341f12556e09
Kévin Casagrande <github.kcasagrande@xoxy.net>
2023-12-17 23:00:15 +0100

    Sudoku validator - Column error - Green

--- src/main/scala/codingame/training/easy/sudokuvalidator/Solution.scala
+++ src/main/scala/codingame/training/easy/sudokuvalidator/Solution.scala
@@ -7,6 +7,12 @@ object Solution extends App {
     _.groupMapReduce(identity[String])(_ => 1)(_ + _).exists(_._2 > 1)
   )
 
+  def hasColumnError(grid: Seq[Seq[String]]): Boolean =
+    grid
+      .flatMap(_.zipWithIndex)
+      .groupMapReduce(identity[(String, Int)])(_ => 1)(_ + _)
+      .exists(_._2 > 1)
+
   val grid = (for {
     _ <- 0 until 9
   } yield {
@@ -14,5 +20,5 @@ object Solution extends App {
   })
     .map(_.split(" ").toSeq)
 
-  println(!hasRowError(grid))
+  println(!hasRowError(grid) && !hasColumnError(grid))
 }
