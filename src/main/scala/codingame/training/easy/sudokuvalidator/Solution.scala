726d42f389daef73aa5954884f8096a095032f40
Kévin Casagrande <github.kcasagrande@xoxy.net>
2023-12-18 15:19:34 +0100

    Sudoku validator - Subgrid error - Green

--- src/main/scala/codingame/training/easy/sudokuvalidator/Solution.scala
+++ src/main/scala/codingame/training/easy/sudokuvalidator/Solution.scala
@@ -13,6 +13,20 @@ object Solution extends App {
       .groupMapReduce(identity[(String, Int)])(_ => 1)(_ + _)
       .exists(_._2 > 1)
 
+  def hasSubgridError(grid: Seq[Seq[String]]): Boolean =
+    grid
+      .map(_.zipWithIndex)
+      .zipWithIndex
+      .flatMap(line => line._1.map {
+        case (value, index) => (value, index, line._2)
+      })
+      .groupBy {
+        case (_, line, row) => (line % 3, row % 3)
+      }
+      .view
+      .mapValues(_.map(_._1).groupMapReduce(identity[String])(_ => 1)(_ + _).exists(_._2 > 1))
+      .exists(_._2)
+
   val grid = (for {
     _ <- 0 until 9
   } yield {
@@ -20,5 +34,5 @@ object Solution extends App {
   })
     .map(_.split(" ").toSeq)
 
-  println(!hasRowError(grid) && !hasColumnError(grid))
+  println(!hasRowError(grid) && !hasColumnError(grid) && !hasSubgridError(grid))
 }
